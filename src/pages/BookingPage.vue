51155a2b1dcd0ad9a964a40f2d38754f02f97531
prog336 <prog663@gmail.com>
2026-06-13 21:19:54 +0300

    Removed unnecessary logging function from booking page

--- src/pages/BookingPage.vue
+++ src/pages/BookingPage.vue
@@ -5,19 +5,13 @@ import BookingsTable from "../components/BookingsTable.vue";
 import BookingsSearchSection from "../components/BookingsSearchSection.vue";
 
 const createDialogVisible = ref(false);
-
-function handleCreateBooking() {
-  console.log("opened create dialog " + createDialogVisible.value);
-  createDialogVisible.value = true;
-  console.log("closed create dialog " + createDialogVisible.value);
-}
 </script>
 
 <template>
   <div class="page-container">
     <h1>Бронирование и оплата</h1>
 
-    <BookingsSearchSection @openBookingsCreateDialog = "handleCreateBooking"/>
+    <BookingsSearchSection @openBookingsCreateDialog = "createDialogVisible = true" />
     <BookingsTable />
 
     <BookingCreateDialog v-if="createDialogVisible" @closeBookingCreateDialog="createDialogVisible = false" />
